12fa9b24cb55e9405610358573feb6295b4e5542
Zarram <thecakeisalie2801@gmail.com>
2020-02-27 10:09:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -55,11 +55,15 @@ function refreshDeviceList(){
 
 
 function onDiscoverDevice(device){
-	//Make a list in html and show devises
+	//Make a list in html and show devises'
+	if(device.name == "meme")
+	{
 		var listItem = document.createElement('li'),
 		html = device.name+ "," + device.id;
 		listItem.innerHTML = html;
 		document.getElementById("bleDeviceList").appendChild(listItem);
+	}
+		
 }
 
 
